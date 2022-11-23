58b21e45cbc32f9af5cc1e35d2938d3140c33381
pandr20 <pandr20@student.sdu.dk>
2022-11-23 22:49:44 +0100

    added png

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
 
+
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
