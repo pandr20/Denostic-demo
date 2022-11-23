9e3fc69a15613e4731616976d6c22d8b0326b75b
pandr20 <pandr20@student.sdu.dk>
2022-11-23 22:39:50 +0100

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -12,8 +12,7 @@ const Testimonials = () => (
       </h2>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
+        Everything you need to have a better patient workflow and grow your business, while having happy patients.
         </p>
       </div>
     </div>
