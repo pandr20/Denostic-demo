033edae337d1d8dd33bd73de83ba4d8c9e4f392f
pandr20 <pandr20@student.sdu.dk>
2022-11-23 22:17:28 +0100

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -14,18 +14,14 @@ const Billing = () => (
 
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        Easily control your <br className="sm:block hidden" /> billing &
-        invoicing
+        Improve patient workflow <br className="sm:block hidden" /> 
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+        Denostic helps you get a faster patient workflow. It gives an easy, reliable and fast diagnosis, that helps you focuse more on the patient.
+        You save time explaining the diagnosis, by having an expert video provided by Denostic, that you can give the patient a link to. 
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
-        <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
       </div>
     </div>
   </section>
