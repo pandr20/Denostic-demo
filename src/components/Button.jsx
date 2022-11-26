a265837f8bc9211c033a7041dae471d113273ffc
pandr20 <pandr20@student.sdu.dk>
2022-11-26 23:08:40 +0100

    Mail works
    Now with subject and body

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,8 @@ import ButtonMailto from "./ButtonMailto";
 
 const Button = ({ styles }) => (
   
-    
-  <button onClick={() => window.location = 'mailto:mw@dentait.dk'} type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+  
+  <button onClick={() => window.location = 'mailto:mw@dentait.dk?subject=I would like to crowdfund&body=I think the project is interesting and would like to crowdfund this project'} type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
     Crowdfund Press here
   </button>
   
