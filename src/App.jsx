c8f2ff0dbfe7e3729437af6f93d064c9a44b4a4c
pandr20 <71690281+pandr20@users.noreply.github.com>
2023-01-29 13:29:15 +0100

    Firebase added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import styles from "./style";
 import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCp8s1q2G2QL4y4wOH2e6TBa99MAeU1VRA",
+  authDomain: "denostic-9e25c.firebaseapp.com",
+  projectId: "denostic-9e25c",
+  storageBucket: "denostic-9e25c.appspot.com",
+  messagingSenderId: "333277952998",
+  appId: "1:333277952998:web:b6e8241dd9dc0a51cbf427",
+  measurementId: "G-4DKBPDKW8Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
